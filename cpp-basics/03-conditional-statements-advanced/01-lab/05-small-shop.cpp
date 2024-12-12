7f48c1e424fd3530e572ff748edc55486b01f872
tonytech83 <a.petrov.83@gmail.com>
2024-12-12 17:12:57 +0200

    init

--- cpp-basics/03-conditional-statements-advanced/01-lab/05-small-shop.cpp
+++ cpp-basics/03-conditional-statements-advanced/01-lab/05-small-shop.cpp
@@ -7,43 +7,43 @@ int main() {
 
     if (town_name == "Sofia") {
         if (product_name == "coffee") {
-            price = quantity * 0.5;
+            price = 0.5;
         } else if (product_name == "water") {
-            price = quantity * 0.80;
+            price = 0.80;
         } else if (product_name == "beer") {
-            price = quantity * 1.2;
+            price = 1.2;
         } else if (product_name == "sweets") {
-            price = quantity * 1.45;
+            price = 1.45;
         } else {
-            price = quantity * 1.6;
+            price = 1.6;
         }
     } else if (town_name == "Plovdiv") {
         if (product_name == "coffee") {
-            price = quantity * 0.4;
+            price = 0.4;
         } else if (product_name == "water") {
-            price = quantity * 0.70;
+            price = 0.70;
         } else if (product_name == "beer") {
-            price = quantity * 1.15;
+            price = 1.15;
         } else if (product_name == "sweets") {
-            price = quantity * 1.30;
+            price = 1.30;
         } else {
-            price = quantity * 1.50;
+            price = 1.50;
         }
     } else {
         if (product_name == "coffee") {
-            price = quantity * 0.45;
+            price = 0.45;
         } else if (product_name == "water") {
-            price = quantity * 0.70;
+            price = 0.70;
         } else if (product_name == "beer") {
-            price = quantity * 1.1;
+            price = 1.1;
         } else if (product_name == "sweets") {
-            price = quantity * 1.35;
+            price = 1.35;
         } else {
-            price = quantity * 1.55;
+            price = 1.55;
         }
     }
 
-    std::cout << price << "\n";
+    std::cout << price * quantity << "\n";
 
     return 0;
 }
