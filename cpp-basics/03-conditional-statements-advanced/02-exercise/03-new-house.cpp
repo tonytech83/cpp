a4a618ee28f806fbcc7ea72e53e2b4d3f86e8927
tonytech83 <a.petrov.83@gmail.com>
2024-12-14 17:10:14 +0200

    Add solutions

--- cpp-basics/03-conditional-statements-advanced/02-exercise/03-new-house.cpp
+++ cpp-basics/03-conditional-statements-advanced/02-exercise/03-new-house.cpp
@@ -1,57 +1,37 @@
 #include <iostream>
 #include <string>
 
-int main()
-{
+int main() {
     std::string flower_type;
     int flower_count, budget;
     std::cin >> flower_type >> flower_count >> budget;
 
     double price;
 
-    if (flower_type == "Roses")
-    {
-        if (flower_count > 80)
-        {
+    if (flower_type == "Roses") {
+        if (flower_count > 80) {
             price = 5 * 0.9;
-        }
-        else
+        } else
             price = 5;
-    }
-    else if (flower_type == "Dahlias")
-    {
-        if (flower_count > 90)
-        {
+    } else if (flower_type == "Dahlias") {
+        if (flower_count > 90) {
             price = 3.80 * 0.85;
-        }
-        else
+        } else
             price = 3.80;
-    }
-    else if (flower_type == "Tulips")
-    {
-        if (flower_count > 80)
-        {
+    } else if (flower_type == "Tulips") {
+        if (flower_count > 80) {
             price = 2.8 * 0.85;
-        }
-        else
+        } else
             price = 2.8;
-    }
-    else if (flower_type == "Narcissus")
-    {
-        if (flower_count < 120)
-        {
+    } else if (flower_type == "Narcissus") {
+        if (flower_count < 120) {
             price = 3 * 1.15;
-        }
-        else
+        } else
             price = 3;
-    }
-    else
-    {
-        if (flower_count < 80)
-        {
+    } else {
+        if (flower_count < 80) {
             price = 2.5 * 1.2;
-        }
-        else
+        } else
             price = 2.5;
     }
 
@@ -60,13 +40,10 @@ int main()
     std::cout.setf(std::ios::fixed);
     std::cout.precision(2);
 
-    if (budget >= total_price)
-    {
+    if (budget >= total_price) {
         std::cout << "Hey, you have a great garden with " << flower_count << " " << flower_type << " and "
                   << budget - total_price << " leva left." << std::endl;
-    }
-    else
-    {
+    } else {
         std::cout << "Not enough money, you need " << total_price - budget << " leva more." << std::endl;
     }
 
