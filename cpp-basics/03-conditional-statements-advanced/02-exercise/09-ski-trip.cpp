6dc0a028430499d6f62a804f37605f19aa6915db
tonytech83 <a.petrov.83@gmail.com>
2024-12-15 08:15:33 +0200

    Add solutions

---
+++ cpp-basics/03-conditional-statements-advanced/02-exercise/09-ski-trip.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <limits>
+#include <string>
+
+int main() {
+    int days_of_stay;
+    std::string rating, room_type;
+
+    std::cin >> days_of_stay;
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    std::getline(std::cin, room_type);
+    std::cin >> rating;
+
+    double price;
+
+    if (days_of_stay < 10) {
+        if (room_type == "room for one person") {
+            price = (days_of_stay - 1) * 18.00;
+        } else if (room_type == "apartment") {
+            price = (days_of_stay - 1) * 25.00 * 0.7;
+        } else {
+            price = (days_of_stay - 1) * 35.00 * 0.9;
+        }
+    } else if (days_of_stay >= 10 && days_of_stay <= 15) {
+        if (room_type == "room for one person") {
+            price = (days_of_stay - 1) * 18.00;
+        } else if (room_type == "apartment") {
+            price = (days_of_stay - 1) * 25.00 * 0.65;
+        } else {
+            price = (days_of_stay - 1) * 35.00 * 0.85;
+        }
+    } else {
+        if (room_type == "room for one person") {
+            price = (days_of_stay - 1) * 18.00;
+        } else if (room_type == "apartment") {
+            price = (days_of_stay - 1) * 25.00 * 0.5;
+        } else {
+            price = (days_of_stay - 1) * 35.00 * 0.8;
+        }
+    }
+
+    if (rating == "positive") {
+        price *= 1.25;
+    } else {
+        price *= 0.9;
+    }
+
+    std::cout.setf(std::ios::fixed);
+    std::cout.precision(2);
+
+    std::cout << price << std::endl;
+
+    return 0;
+}
