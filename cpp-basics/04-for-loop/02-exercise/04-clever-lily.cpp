2a42045b01dfb6f7d1f252791e3507b98c57ea29
tonytech83 <a.petrov.83@gmail.com>
2024-12-16 16:41:17 +0200

    add solutions

--- cpp-basics/04-for-loop/02-exercise/04-clever-lily.cpp
+++ cpp-basics/04-for-loop/02-exercise/04-clever-lily.cpp
@@ -10,8 +10,24 @@ int main() {
 
     std::cin >> age >> washing_machine_price >> toy_price;
 
+    double saved_money = 0;
 
+    for (int year = 1; year <= age; year++) {
+        if (year % 2 == 0) {
+            saved_money += 10 * year / 2 - 1;
+        } else {
+            saved_money += toy_price;
+        }
+    }
 
+    std::cout.setf(std::ios::fixed);
+    std::cout.precision(2);
+
+    if (saved_money >= washing_machine_price) {
+        std::cout << "Yes! " << saved_money - washing_machine_price << std::endl;
+    } else {
+        std::cout << "No! " << washing_machine_price - saved_money << std::endl;
+    }
 
     return 0;
 }
