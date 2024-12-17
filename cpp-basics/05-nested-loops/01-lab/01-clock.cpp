3b3441e7368480d0d66a038e7d1c3f41d043d2ff
tonytech83 <a.petrov.83@gmail.com>
2024-12-17 19:08:09 +0200

    add solutions

---
+++ cpp-basics/05-nested-loops/01-lab/01-clock.cpp
@@ -0,0 +1,15 @@
+//
+// Created by tonytech on 12/17/2024.
+//
+
+#include <iostream>
+
+int main() {
+    for (int i = 0; i <= 23; i++) {
+        for (int j = 0; j <= 59; j++) {
+            std::cout << i << ":" << j << "\n";
+        }
+    }
+
+    return 0;
+}
