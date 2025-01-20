2951549c163686bb04fa6997ee59f3dfbf642c54
tonytech83 <a.petrov.83@gmail.com>
2025-01-20 16:29:39 +0200

    add solutions for "Conditional Statements - Lab"

---
+++ cpp-basics/02-conditional-statements/01-lab/07-area-of-figures.cpp
@@ -0,0 +1,35 @@
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+#include <string>
+
+int main() {
+    std::string type;
+    std::cin >> type;
+
+    std::cout << std::fixed << std::setprecision(3);
+
+    if (type == "square") {
+        double side;
+        std::cin >> side;
+
+        std::cout << side * side << std::endl;
+    } else if (type == "rectangle") {
+        double length, width;
+        std::cin >> length >> width;
+
+        std::cout << length * width << std::endl;
+    } else if (type == "circle") {
+        double radius;
+        std::cin >> radius;
+
+        std::cout << M_PI * radius * radius << std::endl;
+    } else {
+        double length, height;
+        std::cin >> length >> height;
+
+        std::cout << length * height / 2 << std::endl;
+    }
+
+    return 0;
+}
