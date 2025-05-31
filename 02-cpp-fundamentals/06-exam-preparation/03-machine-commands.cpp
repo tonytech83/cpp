5a0e8d1c3a0233c039cb1f459ff1ad191fe8c9c4
tonytech83 <a.petrov.83@gmail.com>
2025-05-31 11:12:41 +0300

    add solution

--- 02-cpp-fundamentals/06-exam-preparation/03-machine-commands.cpp
+++ 02-cpp-fundamentals/06-exam-preparation/03-machine-commands.cpp
@@ -1,3 +1,70 @@
 //
 // Created by tonytech on 30.05.2025.
 //
+#include <iostream>
+#include <list>
+using namespace std;
+
+bool is_number(const string &s) {
+    for (const char c: s)
+        if (!isdigit(c) && c != '-')
+            return false;
+
+    return true;
+}
+
+void sum(list<int> &s) {
+    const int x = s.back();
+    s.pop_back();
+    const int y = s.back();
+    s.pop_back();
+
+    s.push_back(x + y);
+}
+
+void subtract(list<int> &s) {
+    const int x = s.back();
+    s.pop_back();
+    const int y = s.back();
+    s.pop_back();
+
+    s.push_back(x - y);
+}
+
+void concat(list<int> &s) {
+    string x = to_string(s.back());
+    s.pop_back();
+    string y = to_string(s.back());
+    s.pop_back();
+
+    s.push_back(stoi(y + x));
+}
+
+int main() {
+    string input;
+    getline(cin, input);
+
+    list<int> commandQueue;
+
+    while (input != "end") {
+
+        if (is_number(input)) {
+            commandQueue.push_back(stoi(input));
+        } else if (input == "sum") {
+            sum(commandQueue);
+        } else if (input == "subtract") {
+            subtract(commandQueue);
+        } else if (input == "concat") {
+            concat(commandQueue);
+        } else if (input == "discard") {
+            commandQueue.pop_back();
+        }
+
+        getline(cin, input);
+    }
+
+    for (const int i: commandQueue)
+        cout << i << endl;
+
+    return 0;
+}
