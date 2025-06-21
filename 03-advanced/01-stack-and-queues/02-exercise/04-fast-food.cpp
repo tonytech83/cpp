2591145baf0b119476ffcc36fa1120d33d8d1d9b
tonytech83 <a.petrov.83@gmail.com>
2025-06-21 19:51:18 +0300

    add solutions

---
+++ 03-advanced/01-stack-and-queues/02-exercise/04-fast-food.cpp
@@ -0,0 +1,55 @@
+//
+// Created by tonytech on 6/21/25.
+//
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <queue>
+#include <climits>
+using namespace std;
+
+
+int main() {
+    int dayFoodQuantity;
+    cin >> dayFoodQuantity;
+    cin.ignore();
+
+    string buffer;
+    getline(cin, buffer);
+    istringstream iss(buffer);
+
+    queue<int> orders;
+
+    int biggestOrder = INT_MIN;
+    int order;
+    while (iss >> order) {
+        if (order > biggestOrder)
+            biggestOrder = order;
+        orders.push(order);
+    }
+
+    cout << biggestOrder << endl;
+
+    const int ordersSize = orders.size();
+
+    for (int i = 0; i < ordersSize; i++) {
+        int currOrder = orders.front();
+        if (dayFoodQuantity - currOrder >= 0) {
+            orders.pop();
+            dayFoodQuantity -= currOrder;
+        }
+    }
+
+    if (orders.empty())
+        cout << "Orders complete" << endl;
+    else {
+        cout << "Orders left: ";
+
+        while (!orders.empty()) {
+            cout << orders.front() << ' ';
+            orders.pop();
+        }
+    }
+
+    return 0;
+}
