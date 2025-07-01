f02547a5f409885d9e3f2638a932820fde5b7443
tonytech83 <a.petrov.83@gmail.com>
2025-07-01 17:40:23 +0300

    add solutions

--- 03-advanced/03-multidimensional-arrays/02-exercise/06-reverse-matrix-diagonals.cpp
+++ 03-advanced/03-multidimensional-arrays/02-exercise/06-reverse-matrix-diagonals.cpp
@@ -1,3 +1,39 @@
 //
 // Created by tonytech on 30.06.2025.
 //
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+auto readMatrix(vector<vector<int>> &m) -> void {
+    for (auto &row: m)
+        for (auto &col: row)
+            cin >> col;
+}
+
+auto printRightDiagonal(const vector<vector<int>> &m, const int startRow, const int startCol) -> void {
+    const int lastCol = m[0].size() - 1;
+
+    for (int row = startRow, col = startCol; row >= 0 && col <= lastCol; row--, col++)
+        cout << m[row][col] << ' ';
+
+    cout << endl;
+}
+
+auto main() -> int {
+    int rows, cols;
+    cin >> rows >> cols;
+
+    vector matrix(rows, vector<int>(cols));
+    readMatrix(matrix);
+
+    for (int col = matrix[0].size() - 1; col >= 0; col--)
+        printRightDiagonal(matrix, matrix.size() - 1, col);
+
+    for (int row = matrix.size() - 2; row >= 0; row--)
+        printRightDiagonal(matrix, row, 0);
+
+
+    return 0;
+}
