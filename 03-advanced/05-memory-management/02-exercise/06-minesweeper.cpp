531ce0a0858332aa297a6be7183846b598626725
tonytech83 <a.petrov.83@gmail.com>
2025-07-09 19:37:01 +0300

    add solution

--- 03-advanced/05-memory-management/02-exercise/06-minesweeper.cpp
+++ 03-advanced/05-memory-management/02-exercise/06-minesweeper.cpp
@@ -6,16 +6,13 @@
 #include <sstream>
 using namespace std;
 
-auto printField(const shared_ptr<shared_ptr<char[]>[]> &mineField, const int rows, const int cols) -> void {
-    cout << "--------------- Debug start -------------------" << endl;
+auto printField(const shared_ptr<shared_ptr<int[]>[]> &mineField, const int rows, const int cols) -> void {
     for (int r = 0; r < rows; r++) {
         for (int c = 0; c < cols; c++)
             cout << mineField[r][c];
 
         cout << endl;
     }
-
-    cout << "--------------- Debug end -------------------" << endl;
 }
 
 auto readField(shared_ptr<shared_ptr<char[]>[]> &f, const int rows, const int cols) -> void {
@@ -28,46 +25,45 @@ auto readField(shared_ptr<shared_ptr<char[]>[]> &f, const int rows, const int co
     }
 }
 
-auto zeroInitialization(shared_ptr<shared_ptr<char[]>[]> &f, const int rows, const int cols) -> void {
+auto zeroInitialization(shared_ptr<shared_ptr<int[]>[]> &f, const int rows, const int cols) -> void {
     for (int r = 0; r < rows; r++) {
-        f[r] = shared_ptr<char[]>(new char[cols]);
+        f[r] = shared_ptr<int[]>(new int[cols]);
         for (int c = 0; c < cols; c++)
-            f[r][c] = '0';
+            f[r][c] = 0;
     }
 }
 
-auto isInside(const shared_ptr<shared_ptr<char[]>[]> &f, const int rows, const int cols, const int r, const int c)
-        -> bool {
+auto isInside(const int rows, const int cols, const int r, const int c) -> bool {
     if (r >= 0 && r < rows && c >= 0 && c < cols)
-        return f[r][c] == '!';
+        return true;
 
     return false;
 }
 
 
-auto sweep(const shared_ptr<shared_ptr<char[]>[]> &mF, shared_ptr<shared_ptr<char[]>[]> &sF, const int rows,
+auto sweep(const shared_ptr<shared_ptr<char[]>[]> &mF, shared_ptr<shared_ptr<int[]>[]> &sF, const int rows,
            const int cols) -> void {
     for (int r = 0; r < rows; r++)
         for (int c = 0; c < cols; c++)
             if (mF[r][c] == '!') {
-                if (isInside(mF, rows, cols, r - 1, c - 1))
-                    sF[r - 1][c - 1] = '1';
-                if (isInside(mF, rows, cols, r - 1, c))
-                    sF[r - 1][c] = '1';
-                if (isInside(mF, rows, cols, r - 1, c + 1))
-                    sF[r - 1][c + 1] = '1';
-                if (isInside(mF, rows, cols, r, c - 1))
-                    sF[r][c - 1] = '1';
-                if (isInside(mF, rows, cols, r, c))
-                    sF[r][c] = '1';
-                if (isInside(mF, rows, cols, r, c + 1))
-                    sF[r][c + 1] = '1';
-                if (isInside(mF, rows, cols, r + 1, c - 1))
-                    sF[r + 1][c - 1] = '1';
-                if (isInside(mF, rows, cols, r + 1, c))
-                    sF[r + 1][c] = '1';
-                if (isInside(mF, rows, cols, r + 1, c + 1))
-                    sF[r + 1][c + 1] = '1';
+                if (isInside(rows, cols, r - 1, c - 1))
+                    sF[r - 1][c - 1]++;
+                if (isInside(rows, cols, r - 1, c))
+                    sF[r - 1][c]++;
+                if (isInside(rows, cols, r - 1, c + 1))
+                    sF[r - 1][c + 1]++;
+                if (isInside(rows, cols, r, c - 1))
+                    sF[r][c - 1]++;
+                if (isInside(rows, cols, r, c))
+                    sF[r][c]++;
+                if (isInside(rows, cols, r, c + 1))
+                    sF[r][c + 1]++;
+                if (isInside(rows, cols, r + 1, c - 1))
+                    sF[r + 1][c - 1]++;
+                if (isInside(rows, cols, r + 1, c))
+                    sF[r + 1][c]++;
+                if (isInside(rows, cols, r + 1, c + 1))
+                    sF[r + 1][c + 1]++;
             }
 }
 
@@ -77,15 +73,13 @@ auto main() -> int {
 
     // array of shared pointers, which one is pointing to an array of shared pointers
     shared_ptr<shared_ptr<char[]>[]> mineField(new shared_ptr<char[]>[rows]);
-    shared_ptr<shared_ptr<char[]>[]> sweepedField(new shared_ptr<char[]>[rows]);
+    shared_ptr<shared_ptr<int[]>[]> sweepedField(new shared_ptr<int[]>[rows]);
 
     zeroInitialization(sweepedField, rows, cols);
 
     readField(mineField, rows, cols);
     sweep(mineField, sweepedField, rows, cols);
 
-    // Debug
-    // printField(mineField, rows, cols);
     printField(sweepedField, rows, cols);
 
     return 0;
