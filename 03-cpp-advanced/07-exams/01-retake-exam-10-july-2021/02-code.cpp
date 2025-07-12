fb1e20568d63ea20e3bdd5e7ef3ecbecb71be4c1
tonytech83 <a.petrov.83@gmail.com>
2025-07-12 10:54:48 +0300

    add solutions

--- 03-cpp-advanced/07-exams/01-retake-exam-10-july-2021/02-code.cpp
+++ 03-cpp-advanced/07-exams/01-retake-exam-10-july-2021/02-code.cpp
@@ -1,67 +1,54 @@
-//
-// Created by tonytech on 11.07.2025.
-//
-#include <algorithm>
 #include <iostream>
 #include <sstream>
+#include <unordered_map>
+#include <unordered_set>
 #include <vector>
 using namespace std;
 
+auto findNumberFrequency(const vector<int> &message, const unordered_set<int> &separators,
+                         unordered_map<int, int> &freq) -> void {
+    unordered_set<int> part;
 
-auto printCount(const vector<int> &m, const vector<int> &s, const int value) -> void {
-    int counter = 0;
-
-    auto begin = m.begin();
-
-    while (begin != m.end()) {
-        auto end = m.begin();
-        for (int i = static_cast<int>(begin - m.begin()); i != m.size(); i++) {
-            bool foundSeparator = false;
-            for (const int sep: s)
-                if (m[i] == sep) {
-                    end = find(begin, m.end(), m[i]);
-                    foundSeparator = true;
-                    break;
-                }
-            if (foundSeparator)
-                break;
+    for (int num: message) {
+        // if the current number from the message is separator,
+        // we put create key and increment the value into freq map
+        if (separators.count(num)) {
+            for (int value: part)
+                freq[value]++;
+            // clear the current part after it end
+            part.clear();
+        } else {
+            part.insert(num);
         }
-
-        if (count(begin, end, value))
-            counter++;
-
-        begin = (end == m.end()) ? end : end + 1;
     }
 
-    cout << counter << endl;
+    // check the last part for left numbers
+    for (int value: part)
+        freq[value]++;
 }
 
 auto main() -> int {
-    vector<int> separators;
-
-    string sepLine;
-    getline(cin, sepLine);
-    istringstream ss(sepLine);
-
+    unordered_set<int> separators;
+    string line;
+    getline(cin, line);
+    istringstream iss(line);
     int separator;
-    while (ss >> separator)
-        separators.push_back(separator);
+    while (iss >> separator)
+        separators.insert(separator);
 
     vector<int> message;
-
-    string messageLine;
-    getline(cin, messageLine);
-    istringstream iss(messageLine);
-
-
+    getline(cin, line);
+    istringstream msgStream(line);
     int number;
-    while (iss >> number)
+    while (msgStream >> number)
         message.push_back(number);
 
+    unordered_map<int, int> freq;
+    findNumberFrequency(message, separators, freq);
+
     int searchValue;
-    while (cin >> searchValue && searchValue != 0) {
-        printCount(message, separators, searchValue);
-    }
+    while (cin >> searchValue && searchValue != 0)
+        cout << freq[searchValue] << '\n';
 
     return 0;
 }
--- 03-cpp-advanced/07-exams/01-retake-exam-10-july-2021/03-data.cpp
+++ 03-cpp-advanced/07-exams/01-retake-exam-10-july-2021/03-data.cpp
@@ -2,10 +2,85 @@
 // Created by tonytech on 11.07.2025.
 //
 #include <iostream>
+#include <sstream>
+#include <string>
+#include <unordered_map>
+#include <vector>
 using namespace std;
 
+auto readTable(unordered_map<string, vector<unordered_map<string, string>>> &table, const string &indexField) -> void {
+    string line;
+    while (getline(cin, line) && line != "[queries]") {
+        istringstream iss(line);
+        unordered_map<string, string> record;
+        string currKey;
+        string key;
+        string value;
+        while (iss >> key >> value) {
+            record[key] = value;
+
+            if (key == indexField)
+                currKey = value;
+        }
+        if (!currKey.empty())
+            table[currKey].push_back(record);
+    }
+}
+
+auto printData(unordered_map<string, vector<unordered_map<string, string>>> &table) -> void {
+    string query;
+    while (getline(cin, query) && query != "end") {
+        istringstream iss(query);
+        string index, field;
+        iss >> index >> field;
+
+        // Search for the index value in the table. If not found, print "[not found]"
+        auto it = table.find(index);
+        if (it == table.end()) {
+            cout << "[not found]" << endl;
+            continue;
+        }
+
+        // Get a reference to the vector of records `entities` for the found index
+        const auto &entities = it->second;
+        bool printed = false;
+
+        // `entities` is a vector<unordered_map<string, string>>
+        for (const auto &record: entities) {
+            // `record` is an unordered_map<string, string>
+            auto fieldIt = record.find(field); // fieldIt is an iterator to the requested field (if it exists)
+            if (fieldIt != record.end()) {
+                if (printed)
+                    cout << ' ';
+                cout << fieldIt->second;
+                printed = true;
+            }
+        }
+
+        cout << endl;
+    }
+}
+
 auto main() -> int {
-    cout << "test" << endl;
+    string indexField;
+    cin >> indexField;
+    cin.ignore();
+
+    // Example - if index field is age
+    //
+    // name Joro age 25 place unknown
+    // weight 5 name Cat age 25
+    //
+    // //{
+    //  "25" : [
+    //      {"name" : "Joro", "age" : "25", "place" : "unknown"},
+    //      {"weight" : "5", "name" : "Cat", "age" : "25"}
+    //  ]
+    //}
+
+    unordered_map<string, vector<unordered_map<string, string>>> table;
+    readTable(table, indexField);
+    printData(table);
 
     return 0;
 }
