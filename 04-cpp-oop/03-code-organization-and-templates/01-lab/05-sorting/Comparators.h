5ecf922ad773883d77ec6acf0d9e6141488a8b5e
tonytech83 <a.petrov.83@gmail.com>
2025-08-21 08:24:10 +0300

    add lab solution

--- 04-cpp-oop/03-code-organization-and-templates/01-lab/05-sorting/Comparators.h
+++ 04-cpp-oop/03-code-organization-and-templates/01-lab/05-sorting/Comparators.h
@@ -5,4 +5,19 @@
 #ifndef CPP_COMPARATORS_H
 #define CPP_COMPARATORS_H
 
+template<class T>
+class LessThan {
+public:
+    auto operator()(const T &a, const T &b) const -> bool { return a < b; }
+};
+
+template<class T, class Comparator>
+class Reverse {
+public:
+    auto operator()(const T &a, const T &b) const -> bool {
+        Comparator comp;
+        return !comp(a, b);
+    }
+};
+
 #endif // CPP_COMPARATORS_H
--- 04-cpp-oop/03-code-organization-and-templates/01-lab/05-sorting/PrintUtils.h
+++ 04-cpp-oop/03-code-organization-and-templates/01-lab/05-sorting/PrintUtils.h
@@ -5,11 +5,13 @@
 #ifndef CPP_PRINTUTILS_H
 #define CPP_PRINTUTILS_H
 
-#include <set>
+#include <iostream>
 
 template<class T>
-auto printContainer(std::set<T> s.begin(), std::set<T> s) -> void {}
+auto printContainer(class T::iterator start, class T::iterator end) -> void {
+    for (; start != end; ++start)
+        std::cout << *start << ' ';
+    std::cout << std::endl;
+}
 
 #endif // CPP_PRINTUTILS_H
-
-
