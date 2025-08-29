1d6a1a31dd889c68c363f01e5a8a41d3ab941633
tonytech83 <a.petrov.83@gmail.com>
2025-08-29 08:13:08 +0300

    add demos about rule 3/5/0

---
+++ 04-cpp-oop/05-rule-of-3-5-0/00-demos/06-rule-of-zero.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <memory>
+#include <string>
+#include <vector>
+
+class IntArray {
+    std::shared_ptr<int> data;
+    int size;
+
+public:
+    IntArray(int size) : data(new int[size], [](int *p) { delete[] p; }), size(size) {}
+
+    int getSize() const { return this->size; }
+
+    int &operator[](const int index) const { return this->data.get()[index]; }
+};
+
+int main() {
+    while (true) {
+        // no memory leak
+        IntArray arr(10);
+        arr[0] = 42;
+        arr = arr;
+        std::cout << arr[0] << std::endl;
+    }
+
+    return 0;
+}
