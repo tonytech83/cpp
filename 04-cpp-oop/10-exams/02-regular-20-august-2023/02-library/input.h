a2e8842d6a398c860f5c7685c2b80fcdc8b4ca7a
tonytech83 <a.petrov.83@gmail.com>
2025-10-04 10:13:52 +0300

    not solved yet!

--- 04-cpp-oop/10-exams/02-regular-20-august-2023/02-library/input.h
+++ 04-cpp-oop/10-exams/02-regular-20-august-2023/02-library/input.h
@@ -20,20 +20,21 @@ auto read(map<size_t, T> &books, istream &in) -> void {
     }
 }
 
-// For printing all books - declare first
-template<class U>
-auto print(std::map<size_t, U> *const &books, std::ostream &out) -> void {
-    for (const auto &pair : *books) {
-        out << "book " << pair.second.getId() << " " << pair.second.getTitle() << " by "
-            << pair.second.getAuthor() << " is " << pair.second.getBorrower() << std::endl;
-    }
+template<class T>
+ auto print(const T &item, std::ostream &out) -> void {
+    out << "book " << item.getId()
+        << " " << item.getTitle()
+        << " by " << item.getAuthor()
+        << " is " << (item.getBorrower().empty() ? "available" : item.getBorrower())
+        << std::endl;
 }
 
-// For printing a single book
+
 template<class T>
-auto print(const T &item, std::ostream &out) -> void {
-    out << "book " << item.getId() << " " << item.getTitle() << " by "
-        << item.getAuthor() << " is " << item.getBorrower() << std::endl;
+auto print(std::map<size_t, T>* books, std::ostream &out) -> void {
+    for (auto &p : *books) {
+        print(p.second, out);
+    }
 }
 
 template<class T>
